03411d846b7e7956aaebdfe0738274304da6ce14
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-14 23:11:24 -0400

    Update build.sh

--- 0.3.5/build.sh
+++ 0.3.5/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 IMAGE=singularity-msee-0.3.5.sif
 DEFINITION=Singularity
 
 if [ -f $IMAGE ]; then
-        rm -fv $IMAGE
+	rm -fv $IMAGE
 fi
 
 sudo singularity build $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
